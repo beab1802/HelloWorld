e328f70cf17294600149b360cdf58058bdbf9bb5
bernard <bernard.beaulieu@usherbrooke.ca>
2011-03-23 13:03:38 -0400

    test

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -6,7 +6,7 @@ public class HelloJava {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld"); 
 
 	}
 
